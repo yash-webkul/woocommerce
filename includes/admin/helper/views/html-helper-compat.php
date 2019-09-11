1e41f15cb7fea165e4e483638012d021ce123422
Yash Kapoor <52190071+yash-webkul@users.noreply.github.com>
2019-09-11 15:53:19 +0530

    Added escaping

--- includes/admin/helper/views/html-helper-compat.php
+++ includes/admin/helper/views/html-helper-compat.php
@@ -1,6 +1,6 @@
 <?php defined( 'ABSPATH' ) or exit(); ?>
 
 <div class="wrap">
-	<h1><?php _e( 'Looking for the WooCommerce Helper?', 'woocommerce' ); ?></h1>
-	<p><?php printf( __( 'We\'ve made things simpler and easier to manage moving forward. From now on you can manage all your WooCommerce purchases directly from the Extensions menu within the WooCommerce plugin itself. <a href="%s">View and manage</a> your extensions now.', 'woocommerce' ), esc_url( $helper_url ) ); ?></p>
+	<h1><?php esc_html_e( 'Looking for the WooCommerce Helper?', 'woocommerce' ); ?></h1>
+	<p><?php sprintf( esc_html__( 'We\'ve made things simpler and easier to manage moving forward. From now on you can manage all your WooCommerce purchases directly from the Extensions menu within the WooCommerce plugin itself. %s your extensions now.', 'woocommerce' ), '<a href="' . esc_url( $helper_url ) . '">' . esc_html( 'View and manage', 'woocommerce' ) . '</a>' ); ?></p>
 </div>
